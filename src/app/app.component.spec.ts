97a3daa1c54df8b11306a5d26fafefb4a3d56f6d
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2025-04-06 13:32:09 +0900

    test: simplify withtesting-library

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,13 @@
-import { TestBed } from '@angular/core/testing';
+import { render, screen } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  it('アプリが正しくレンダリングされ、タイトルが表示されること', async () => {
+    // コンポーネントをレンダリング
+    await render(AppComponent);
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'template-angular-19' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('template-angular-19');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome to template-angular-19',
-    );
+    // h1要素内にアプリのタイトルが含まれているか確認
+    const heading = screen.getByRole('heading', { level: 1 });
+    expect(heading.textContent).toContain('Welcome to template-angular-19');
   });
 });
